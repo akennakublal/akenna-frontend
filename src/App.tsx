50493cffb817880a6df044d8a3694510d54a730f
Sa-Yoor Headley <sai.h152015@gmail.com>
2025-09-10 21:16:44 -0400

    Added font-body to App

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import AppRoutes from "./AppRoutes";
 
 function App() {
   return (
-    <div className="p-0 m-0">
+    <div className="font-body p-0 m-0">
       <Router>
         <AppRoutes />
       </Router>
